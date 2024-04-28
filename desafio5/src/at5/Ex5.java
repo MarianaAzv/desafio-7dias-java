0b7bc8346727375481d392a46562b87e64acba47
Mariana <mariana.a2006@aluno.ifsc.edu.br>
2024-04-28 17:05:41 -0300

    Dia 5

---
+++ desafio5/src/at5/Ex5.java
@@ -0,0 +1,29 @@
+package at5;
+import java.util.Scanner;
+
+public class Ex5 {
+
+
+
+		public static void main(String[] args) {
+			
+			Scanner ler =  new Scanner(System.in);
+				
+			
+			int[][] matriz = new int[10][10];
+
+				
+			for (int i = 0; i < 10; i++) {
+				for (int j = 0; j < 10; j++) {
+					System.out.println( matriz[i][j] + "0" );
+					
+				}
+		System.out.println();
+			}
+			
+					
+		}
+
+	}
+
+
